4c775eff4baf32b65de091f8b1726458c1253da7
Devang Thaker <devangt101003@gamil.com>
2024-03-01 13:03:55 +0530

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>MediConnect</h1>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path = "/login" element = {<Login />}/>
+          <Route path = "/register" element = {<Register />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
